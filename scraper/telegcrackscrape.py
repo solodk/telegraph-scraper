066fc4cf195d7c68e5a4f2edd67146b67374e906
solodk <solodk.m@gmail.com>
2023-09-09 17:22:35 +0300

    Added progress bar, code enhancements

--- scraper/telegcrackscrape.py
+++ scraper/telegcrackscrape.py
@@ -12,15 +12,24 @@ class Scraper(object):
     def __init__(self, query):
         self.input_query = query
         self.session = requests.Session()
-        self.cache_file = self.input_query + '_cache_store'
+        self.cache_file = query + '_cache_store'
         self.cache_path = os.path.join(os.path.dirname(__file__), 'cache', self.cache_file)
         self.currentdate = datetime.now()
-        self.filtered_pages = []
+        root = os.getcwd()
+        self.query_path = os.path.join(root, query)
+        if not os.path.exists(self.query_path):
+            os.makedirs(self.query_path)
+        os.chdir(self.query_path)
         self.getCache()
         self.indexQuery()
         self.updateCache()
     
     def getCache(self):
+        """
+        Loads cached data from the cache file or initializes an empty cache if none is found
+        :params: none
+        :return: none
+        """
         try:
             with open(self.cache_path, 'r', encoding='utf-8') as f:
                 f.seek(0)
@@ -31,7 +40,9 @@ class Scraper(object):
 
     def updateCache(self):
         '''
-        This function is meant to update the current used cache file with any new information
+        Updates the cache with the current page list and date and writes it to the cache file
+        :params: none
+        :return: none
         '''
         self.cache = {
             'pagelist':self.pagelist[:],
@@ -46,7 +57,11 @@ class Scraper(object):
             json.dump(self.cache, f, ensure_ascii=False, indent=4, default=str)
     
     def newQuery(self):
-
+        '''
+        Sets up the start and end dates for the query and initializes an empty or cached page list
+        :params: none
+        :return: none
+        '''
         if self.cache:
             self.start_date = datetime.fromisoformat(self.cache['date'])
             self.end_date = self.currentdate
@@ -58,12 +73,19 @@ class Scraper(object):
 
     def indexQuery(self):
         '''
-        Makes a list of pages by using getJSON() func for a single query
+        Iterates through a range of dates to fetch pages using the search query, appending valid pages to the pagelist
         :params: none
         :return: none
         '''
         index = 1
-
+        delta = self.end_date - self.start_date
+        self.outer_pbar = tqdm(
+                total=delta.days,
+                desc=f'Indexing "{self.input_query}" pages',
+                unit=' page',
+                position=0,
+                leave=True
+            )
         while self.start_date < self.end_date:
             formatted_date = self.start_date.strftime('%m-%d')
             search_query = [self.input_query, formatted_date]
@@ -81,14 +103,16 @@ class Scraper(object):
                 else:
                     self.start_date += timedelta(days=1)
                     index = 1 #Reset the index
+                    self.outer_pbar.update()
             except Exception as ex:
                 print(f'indexQuery(): Error at {search_query}')
+        self.outer_pbar.close()    
                 
     def getJSON(self, search_query):
         '''
-        Searching for a page by a single query
-        :params: none
-        :return: json answer from api
+        Sends an HTTP request to the Telegraph API to fetch page data for a given search query
+        :params: search_query (str) - The search query for a single page
+        :return: JSON response from the API
         '''
         try:
             result = self.session.get(
@@ -98,19 +122,50 @@ class Scraper(object):
             print(f'getJSON(): Failed to get {search_query}')
         return result
 
-    def getImages(self, page):
+    def getImages(self):
         '''
-        downloads a images of one page
+        Downloads images from indexed pages and stores them in corresponding directories
+        :params: none
+        :return: none
         '''
-        self.getImageList(page)
+        self.outer_pbar = tqdm(
+                total=len(self.pagelist),
+                desc=f'Scrapping images from "{self.input_query}" pages',
+                unit=' page',
+                position=0,
+                leave=True
+            )
+        for page in self.pagelist:
+            page_name = page['result']['path']
+            page_path = os.path.join(self.query_path, page_name)
+            if not os.path.exists(page_path):
+                os.makedirs(page_path)
+            os.chdir(page_path)
+            
+            self.getImageList(page)
 
-        for index, file in enumerate(self.imagelist, start=1):
-            with open(f'{index}.jpg', 'wb') as f:
-                f.write(requests.get(f'https://telegra.ph/file/{file}', stream=True).content)
+            self.inner_pbar = tqdm(
+                total=len(self.imagelist),
+                desc=f'Downloading images from "{page_name}" page',
+                unit=' image',
+                position=1,
+                leave=False
+            )
+            for index, file in enumerate(self.imagelist, start=1):
+                with open(f'{index}.jpg', 'wb') as f:
+                    f.write(requests.get(f'https://telegra.ph/file/{file}', stream=True).content)
+                self.inner_pbar.update()
+            self.inner_pbar.close()
+
+            self.outer_pbar.update()
+        self.outer_pbar.close()
+            
     
     def getImageList(self, page):
         '''
-        Takes a names of images on a page into list
+        Extracts image URLs from the content of a given page and populates the imagelist
+        :params: page (dict) - JSON data for a telegraph page
+        :return: None
         '''
         self.imagelist = []
         content = page['result']['content']
@@ -120,21 +175,42 @@ class Scraper(object):
                     if sub_tag['tag'] == 'img':
                         self.imagelist.append(sub_tag['attrs']['src'][6:])
 
-    def getText(self, page):
+    def getText(self):
         '''
-        Gather all text from page and saves it into file
+        Gathers all text content from indexed pages and saves it into text files
+        :params: none
+        :return: none
         '''
-        self.getTextList(page)
+        self.outer_pbar = tqdm(
+                total=len(self.pagelist),
+                desc=f'Scrapping text from "{self.input_query}" pages',
+                unit=' page',
+                position=0,
+                leave=True
+            )
+        for page in self.pagelist:
+            page_name = page['result']['path']
+            page_path = os.path.join(self.query_path, page_name)
+            if not os.path.exists(page_path):
+                os.makedirs(page_path)
+            os.chdir(page_path)
+            
+            self.getTextList(page)
 
-        self.textlist = list(filter(None, self.textlist)) #filter empty values from textlist
-        if self.textlist:
-            with open('text.txt', 'w', encoding='utf-8') as f:
-                for line in self.textlist:
-                    f.write(f'{line}\n')
+            self.textlist = list(filter(None, self.textlist)) #filter empty values from textlist
+            if self.textlist:
+                with open('text.txt', 'w', encoding='utf-8') as f:
+                    for line in self.textlist:
+                        f.write(f'{line}\n')
+
+            self.outer_pbar.update()
+        self.outer_pbar.close()
 
     def getTextList(self, page):
         '''
-        Collect all text form page into list
+        Collects all text from a given page and stores it in the textlist
+        :params: page (dict) - JSON data for a telegraph page.
+        :return: None
         '''
         self.textlist = []
         content = page['result']['content']
@@ -145,20 +221,41 @@ class Scraper(object):
                         if isinstance(child, str):
                             self.textlist.append(child.replace('\n', ' ').strip())
     
-    def getLinks(self, page):
+    def getLinks(self):
         '''
-        Gather all links from page and saves it into file
+        Gathers all links from indexed pages and saves them into a text file
+        :params: page (dict) - JSON data for a telegraph page.
+        :return: none
         '''
-        self.getLinksList(page)
+        self.outer_pbar = tqdm(
+                total=len(self.pagelist),
+                desc=f'Scrapping links from "{self.input_query}" pages',
+                unit=' page',
+                position=0,
+                leave=True
+            )
+        for page in self.pagelist:
+            page_name = page['result']['path']
+            page_path = os.path.join(self.query_path, page_name)
+            if not os.path.exists(page_path):
+                os.makedirs(page_path)
+            os.chdir(page_path)
+            
+            self.getLinksList(page)
 
-        if self.linklist:
-            with open('links.txt', 'w', encoding='utf-8') as f:
-                for line in self.linklist:
-                    f.write(f'{line}\n')
+            if self.linklist:
+                with open('links.txt', 'w', encoding='utf-8') as f:
+                    for line in self.linklist:
+                        f.write(f'{line}\n')
+
+            self.outer_pbar.update()
+        self.outer_pbar.close()
 
     def getLinksList(self, page):
         '''
-        Collect all links form page into list
+        Collects all links from a given page and stores them in the linklist
+        :params: page (dict) - JSON data for a telegraph page.
+        :return: none
         '''
         self.linklist = []
         content = page['result']['content']
@@ -170,13 +267,24 @@ class Scraper(object):
                         #dict['attrs']['href']
                         self.linklist.append(dict['attrs']['href'])
 
+    def getPagesUrl(self):
+        '''
+        Collects and stores the URLs of all indexed pages in a text file
+        :params: none
+        :return: none
+        '''
+        links = [page['result']['url'] for page in self.pagelist]
+        os.chdir(self.query_path)
+        with open(f'{self.input_query}.txt', 'w') as f:
+            for link in links:
+                f.write(f'{link}\n')
+
     def filterSpam(self):
         '''
-        Checks if page ok for current filters
-        :params: page in json
-        :return: true if author name is not in spamlist
+        Filters out pages with authors in a predefined spam list from the pagelist
+        :params: none
+        :return: none
         '''
-        
         for page in self.pagelist[:]:
             author_name = page.get('result', {}).get('author_name')
             if author_name is not None and author_name in extra.spam:
@@ -184,9 +292,9 @@ class Scraper(object):
 
     def filterText(self, min_length, max_length):
         '''
-        Checks if page ok for current filters
-        :params: page in json
-        :return: true if text length in range of min and max
+        Filters pages based on text length criteria, removing pages falling outside the specified range
+        :params: min_length (int, optional) - Minimum text length; max_length (int, optional) - Maximum text length.
+        :return: none
         '''
         for page in self.pagelist[:]:
             self.getTextList(page)
@@ -210,7 +318,6 @@ def parser():
     main_grp.add_argument('-i', '--input-file', help = '<INPUT_FILE> text file containing the target list. Ex: list.txt')
     main_grp.add_argument('-o', '--output-directory', help = '<OUTPUT_DIRECTORY> (optional): query output directory (default \'./<QUERY>/\')')
     main_grp.add_argument('-w', '--workers', help = '<WORKERS> (optional): number of parallel execution workers (default 4)', default = 4)
-    main_grp.add_argument('-c', '--cache', action='store_true', help = 'Use cached results')
 
     output_grp = parser.add_argument_group('Output parameters')
     output_grp.add_argument('-I', '--images', action='store_true', help = 'collect all images on indexed pages')
@@ -235,49 +342,30 @@ def main():
         input_list = [args.QUERY]
     
     if args.output_directory:
-        root = os.path.abspath(args.output_directory)
-    else:
-        root = os.getcwd()
+        os.chdir(os.path.abspath(args.output_directory))
 
     if args.workers:
         pass
-
+    
     for query in input_list:
-        telegraph = Scraper(query)
+        scraper = Scraper(query)
         
-        telegraph.filterSpam()
+        scraper.filterSpam()
 
         if args.min or args.max:
-            telegraph.filterText(args.min, args.max)
+            scraper.filterText(args.min, args.max)
 
-        if args.images or args.text or args.links:
-            query_path = os.path.join(root, query)
-            if not os.path.exists(query_path):
-                os.makedirs(query_path)
+        if args.images:
+            scraper.getImages()
+        
+        if args.text:
+            scraper.getText()
+        
+        if args.links:
+            scraper.getLinks()
+                
+        if not args.images or not args.text or not args.links:
+            scraper.getPagesUrl()            
 
-            for page in telegraph.pagelist:
-                page_name = page['result']['path']
-                page_path = os.path.join(query_path, page_name)
-                if not os.path.exists(page_path):
-                    os.makedirs(page_path)
-                os.chdir(page_path)
-                
-                if args.images:
-                    telegraph.getImages(page)
-                
-                if args.text:
-                    telegraph.getText(page)
-                
-                if args.links:
-                    telegraph.getLinks(page)
-                
-        else:
-            links = [page['result']['url'] for page in telegraph.pagelist]
-            with open(f'{query}.txt', 'w') as f:
-                for link in links:
-                    f.write(f'{link}\n')
-
-        os.chdir(root)
-      
 if __name__ == '__main__':
     main()
